cb3b2bba5164887dc76833b1e36508a10ea5ebb1
Marcus Pettersen <Marcus Pettersen>
2017-01-28 09:31:59 +0100

    Initial commit

---
+++ RestJson/IRestJsonConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skogsaas.Monolith.Plugins.RestJson
+{
+    public interface IRestJsonConfig : Configuration.Identifier
+    {
+        int Port { get; set; }
+        List<string> Channels { get; set; }
+    }
+}
---
+++ RestJson/RestJsonServer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Skogsaas.Monolith.Plugins;
+using Skogsaas.Legion;
+using System.Collections.Generic;
+
+namespace Skogsaas.Monolith.Plugins.RestJson
+{
+    public class RestJsonServer : IPlugin
+    {
+        private Skogsaas.Legion.RestJson.Server server;
+
+        private Channel configChannel; 
+
+        public void initialize()
+        {
+            this.configChannel = Manager.Create(Configuration.Constants.Channel);
+            this.configChannel.SubscribePublish(typeof(IRestJsonConfig), onConfigPublished);
+        }
+
+        private void onConfigPublished(Channel c, IObject obj)
+        {
+            if(this.server == null)
+            {
+                IRestJsonConfig config = (IRestJsonConfig)obj;
+
+                List<Channel> channels = new List<Channel>();
+
+                foreach(string name in config.Channels)
+                {
+                    channels.Add(Manager.Create(name));
+                }
+
+                this.server = new Skogsaas.Legion.RestJson.Server(config.Port, channels.ToArray());
+                this.server.Start();
+            }
+        }
+    }
+}
